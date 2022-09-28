08c3f3189ecb313a0535a013ef3bf9e0e011b017
Arinze <arinzebenneth@gmail.com>
2022-09-28 04:50:18 -0700

    first commit

---
+++ first_class.py
@@ -0,0 +1,38 @@
+# print(type("4.5"))
+
+# my_number = 50
+# my_num = 10
+
+# print(my_number/my_num)
+
+# a= 4
+# b = 10
+
+# x = 4*b - 2*a
+
+# print(x)
+
+# nums = (2,4,9,8,7,2)
+# average = 2+4+9+8+7+2 // 6
+# print(average)
+
+# from re import A
+
+
+# a = 4
+# b = 7
+# c = 2
+
+# numerator = (-b) + (4*a*C)**0.5
+# denominator = 2*a
+
+# x = numerator/denominator
+
+# print (x)
+
+tomi = 20
+y = 10
+
+x = tomi+y
+
+print(x)
